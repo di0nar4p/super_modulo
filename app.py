11bccec62977473831a259da1c62ef4e2a6e5904
Eduardo_B <eduardo.filhodorei14@gmail.com>
2023-09-09 12:59:47 -0300

    app novo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ for i in range(1, 4+1):
     nota = float(input(f" Digite a {i}ª nota do aluno: "))
     count += nota 
 media = count / 4
-print(media)
+print(f"A media do aluno é {media}")
 
 if media >= 10:
     print("Aprovado com louvor!!")

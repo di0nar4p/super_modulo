51ddf2789d113c6f4fe3404ba0a6a1c297a3b097
Eduardo_B <eduardo.filhodorei14@gmail.com>
2023-09-09 12:48:39 -0300

    app novo

---
+++ app.py
@@ -0,0 +1,16 @@
+count = 0
+print(f"{'*' * 15} Nota media do aluno {'*' * 15}")
+for i in range(1, 4+1):
+    nota = float(input(f" Digite a {i}ª nota do aluno: "))
+    count += nota 
+media = count / 4
+print(media)
+
+if media >= 10:
+    print("Aprovado com louvor!!")
+elif media >= 7:
+    print("Aprovado :)")
+elif media == 5: 
+    print("Recuperação")
+elif media < 5:
+    print("Reprovado")
